c6244d4c45d39e0161e069670b34e4f9af063e4d
Artemis1799 <enobeliche@gmail.com>
2025-11-19 20:55:11 +0100

    fix cors error

--- Program.cs
+++ Program.cs
@@ -8,6 +8,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+
+// CORS: allow the Angular dev server at http://localhost:4200
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 builder.Services.AddScoped<IEventService, EventService>();
 builder.Services.AddScoped<IEquipmentService, EquipmentService>();
 builder.Services.AddScoped<IPhotoService, PhotoService>();
@@ -15,6 +27,8 @@ builder.Services.AddScoped<IPointService, PointService>();
 builder.Services.AddScoped<IImagePointService, ImagePointService>();
 
 var app = builder.Build();
+// enable CORS
+app.UseCors("AllowAngularDev");
 
 app.MapControllers();
 app.Run();
