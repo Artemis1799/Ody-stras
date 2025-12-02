81d27a29eaa87b68b2ed867ab0d917d1891eabb4
VALLET LUCA <luca.vallet@etu.unistra.fr>
2025-12-02 01:06:16 +0100

    feat: new migrations

---
+++ Migrations/20251201200711_RenameGeometryJsonColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace t5_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameGeometryJsonColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Properties",
+                table: "Geometries",
+                newName: "PropertiesString");
+
+            migrationBuilder.RenameColumn(
+                name: "GeoJson",
+                table: "Geometries",
+                newName: "GeoJsonString");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PropertiesString",
+                table: "Geometries",
+                newName: "Properties");
+
+            migrationBuilder.RenameColumn(
+                name: "GeoJsonString",
+                table: "Geometries",
+                newName: "GeoJson");
+        }
+    }
+}
