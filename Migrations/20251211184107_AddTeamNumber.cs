4785739f310a6e7f161c129d91ac5e5f8469e2f9
VALLET LUCA <luca.vallet@etu.unistra.fr>
2025-12-12 13:34:45 +0100

    feat : add numbers on teams

---
+++ Migrations/20251211184107_AddTeamNumber.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace t5_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeamNumber",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeamNumber",
+                table: "Teams");
+        }
+    }
+}
